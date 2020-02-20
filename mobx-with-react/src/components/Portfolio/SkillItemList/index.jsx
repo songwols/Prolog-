008f6210bc1268fb696166dfbd94640f71c6f38a
조경은 <ruddms0127@naver.com>
2020-02-20 09:57:17 +0900

    error해결

--- mobx-with-react/src/components/Portfolio/SkillItemList/index.jsx
+++ mobx-with-react/src/components/Portfolio/SkillItemList/index.jsx
@@ -1,28 +1,22 @@
-import React, { Component } from 'react';
-import SkillItem from '../SkillItem';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import SkillItem from "../SkillItem";
+import styled from "styled-components";
 
 class SkillItemList extends Component {
   render() {
-    let skillList = undefined
-    if(this.props.skills){
-      skillList  = this.props.skills.map((item,index)=> (
+    let skillList = undefined;
+    if (this.props.skills) {
+      skillList = this.props.skills.map((item, index) => (
         <SkillItem key={index} text={item.value} />
       ));
+    } else {
+      skillList = <div></div>;
     }
-    else{
-      skillList = (<div></div>)
-    }
-    
-    return (
-      <SkillItemListLayout>
-        {skillList}       
-      </SkillItemListLayout>
-    );
+
+    return <SkillItemListLayout>{skillList}</SkillItemListLayout>;
   }
 }
 
-const SkillItemListLayout = styled.div`
-`
+const SkillItemListLayout = styled.div``;
 
 export default SkillItemList;
